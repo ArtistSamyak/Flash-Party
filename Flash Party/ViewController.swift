81ee38a9c6fec55680cd3918007abe4ccd5b0e3f
ArtistSamyak <samyak.professional@gmail.com>
2020-07-14 09:47:23 +0530

    final

--- Flash Party/ViewController.swift
+++ Flash Party/ViewController.swift
@@ -7,13 +7,74 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
+    var rate: Float = 0.7
+    var timer : Timer? = nil
+    let colors = [ #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "8"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "6"), #imageLiteral(resourceName: "7")]
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateRate(rate: rate)
     }
+    
+    
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+    
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        rate = sender.value
+        print(rate)
+        updateRate(rate: rate)
+        
+    }
+    
+    func updateRate(rate : Float) {
+        var isOn = false
+        timer?.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: Double(1 - rate), repeats: true) { timer in
+            
+            self.image.image = self.colors.randomElement()
+            
+            if isOn == true {
+                
+                self.toggleTorch(on: false)
+                isOn = false
+            }else if isOn == false{
+                
+                isOn = true
+                self.toggleTorch(on: true)
+            }
+            
+        }
+    }
+    
 
 
 }
